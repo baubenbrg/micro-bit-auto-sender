f371647eae804c314fee9737c23c47807468313b
baubenbrg <78410230+baubenbrg@users.noreply.github.com>
2021-02-25 07:59:56 +0100

    02 Etwas anzeigen und erste Nachrichten versenden

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.North)
+    radio.sendString("langsamer")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.South)
+    radio.sendString("zurück")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.North)
+    radio.sendString("schneller")
+})
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -5,6 +17,7 @@ basic.showLeds(`
     . . # . .
     . . . . .
     `)
+radio.setGroup(49)
 basic.forever(function () {
 	
 })
