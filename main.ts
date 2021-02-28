33f667b4b1ef38c904abdefa4986865639cd7766
baubenbrg <78410230+baubenbrg@users.noreply.github.com>
2021-02-28 22:45:20 +0100

    Lenkung

--- main.ts
+++ main.ts
@@ -10,6 +10,17 @@ input.onButtonPressed(Button.B, function () {
     basic.showArrow(ArrowNames.North)
     radio.sendString("schneller")
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    radio.sendString("stop")
+})
+let r = 0
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -19,5 +30,7 @@ basic.showLeds(`
     `)
 radio.setGroup(49)
 basic.forever(function () {
-	
+    r = 180 * Math.atan2(input.acceleration(Dimension.Y), -1 * input.acceleration(Dimension.X)) - 90
+    r = Math.constrain(r, -45, 45)
+    radio.sendNumber(r)
 })
