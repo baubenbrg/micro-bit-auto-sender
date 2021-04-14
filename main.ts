b5329184b8e87bb8f366791406ebc2f91621dd1f
baubenbrg <78410230+baubenbrg@users.noreply.github.com>
2021-04-14 08:16:36 +0200

    '28.2.2021, 22:45:33 1.0.0 ' wiederherstellen '

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.North)
+    radio.sendString("langsamer")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showArrow(ArrowNames.South)
+    radio.sendString("zurück")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showArrow(ArrowNames.North)
+    radio.sendString("schneller")
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    radio.sendString("stop")
+})
+let r = 0
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -5,6 +28,9 @@ basic.showLeds(`
     . . # . .
     . . . . .
     `)
+radio.setGroup(49)
 basic.forever(function () {
-	
+    r = 180 * Math.atan2(input.acceleration(Dimension.Y), -1 * input.acceleration(Dimension.X)) - 90
+    r = Math.constrain(r, -45, 45)
+    radio.sendNumber(r)
 })
